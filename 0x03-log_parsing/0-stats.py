7c09b48f6ae1e99361dc1bb069fca2a296709a7e
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-08-18 05:26:08 +0000

    add docs to generator

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,20 +2,21 @@
 """
 Log parsing module.
 To add robust checking using regular expression.
-
 """
-import re
 import sys
+
+
 lineNumber = 0
 currentStats = {}
 fileSize = 0
-try:
-    for line in sys.stdin:
+for line in sys.stdin:
+    try:
         a = line.split(" ")
         try:
             code = float(a[-2])
             assert(code in [200, 301, 400, 401, 403, 404, 405, 500])
             code = int(code)
+            assert(len(a) == 9)
             number = int(a[-1])
             assert(a[-3] == 'HTTP/1.1"')
             assert(a[-4] == '/projects/260')
@@ -30,12 +31,12 @@ try:
             break
         except Exception as e:
             if e == KeyboardInterrupt:
-                raise KeyboardInterrupt
+                pass
         if lineNumber % 10 == 0:
             print("File size: {}".format(fileSize))
             for key in sorted(currentStats):
                 print("{}: {}".format(key, currentStats[key]))
-except KeyboardInterrupt:
-    print("File size: {}".format(fileSize))
-    for key in sorted(currentStats):
-        print("{}: {}".format(key, currentStats[key]))
+    except KeyboardInterrupt:
+        print("File size: {}".format(fileSize))
+        for key in sorted(currentStats):
+            print("{}: {}".format(key, currentStats[key]))
