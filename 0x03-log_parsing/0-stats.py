3a17f4e86d7f281498c166424cd0bf5ebfde0684
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-08-18 03:57:05 +0000

    added key interrupt handler outside of for loop and forward keyboard interrupt and ignore other exceptions

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,28 +9,34 @@ import sys
 lineNumber = 0
 currentStats = {}
 fileSize = 0
-while True:
-    line = sys.stdin.readline()
-    a = line.split(" ")
-    try:
-        code = float(a[-2])
-        assert(code in [200, 301, 400, 401, 403, 404, 405, 500])
-        code = int(code)
-        number = int(a[-1])
-        assert(a[-3] == 'HTTP/1.1"')
-        assert(a[-4] == '/projects/260')
-        assert(a[-5] == '"GET')
-        currentStats[code] = currentStats.get(code, 0) + 1
-        fileSize += number
-        lineNumber += 1
-    except KeyboardInterrupt:
-        print("File size: {}".format(fileSize))
-        for key in sorted(currentStates):
-            print("{}: {}".format(key, currentStats[key]))
-        break
-    except Exception as e:
-        pass
-    if lineNumber % 10 == 0:
-        print("File size: {}".format(fileSize))
-        for key in sorted(currentStats):
-            print("{}: {}".format(key, currentStats[key]))
+try:
+    while True:
+        line = sys.stdin.readline()
+        a = line.split(" ")
+        try:
+            code = float(a[-2])
+            assert(code in [200, 301, 400, 401, 403, 404, 405, 500])
+            code = int(code)
+            number = int(a[-1])
+            assert(a[-3] == 'HTTP/1.1"')
+            assert(a[-4] == '/projects/260')
+            assert(a[-5] == '"GET')
+            currentStats[code] = currentStats.get(code, 0) + 1
+            fileSize += number
+            lineNumber += 1
+        except KeyboardInterrupt:
+            print("File size: {}".format(fileSize))
+            for key in sorted(currentStats):
+                print("{}: {}".format(key, currentStats[key]))
+            break
+        except Exception as e:
+            if e == KeyboardInterrupt:
+                raise KeyboardInterrupt
+        if lineNumber % 10 == 0:
+            print("File size: {}".format(fileSize))
+            for key in sorted(currentStats):
+                print("{}: {}".format(key, currentStats[key]))
+except KeyboardInterrupt:
+    print("File size: {}".format(fileSize))
+    for key in sorted(currentStats):
+        print("{}: {}".format(key, currentStats[key]))
