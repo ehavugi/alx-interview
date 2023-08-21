859766e56a1d2481a506de7dbc76526e1ad3b0fc
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-08-21 08:13:28 +0000

    added type ananotation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,8 +2,10 @@
 """
 The utf-8 validation module
 """
+from typing import List
 
-def validUTF8(data):
+
+def validUTF8(data: List[int]) -> bool:
     """return True if data is utf-8 otherwise False
     """
     return True
