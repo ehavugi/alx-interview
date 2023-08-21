80cefe2249a1617844bedebb6ceb64f634ce8dd8
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-08-21 02:52:57 +0000

    added ctrl+c handling with signals

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,47 @@
 #!/usr/bin/python3
 """
 Log parsing module.
-To add robust checking using regular expression.
+To add robust checking using regular expression
 """
+
 import sys
+import signal
 
 
 lineNumber = 0
 currentStats = {}
 fileSize = 0
+
+
+def printstats(sig=0, frame=0):
+    """print stats
+    """
+    print("File size: {}".format(fileSize))
+    for key in sorted(currentStats):
+        print("{}: {}".format(key, currentStats[key]))
+
+
+signal.signal(signal.SIGINT, printstats)
+
 for line in sys.stdin:
     try:
         a = line.split(" ")
-        try:
-            code = float(a[-2])
-            assert(code in [200, 301, 400, 401, 403, 404, 405, 500])
+        code = float(a[-2])
+        if (code in [200, 301, 400, 401, 403, 404, 405, 500]):
             code = int(code)
-            assert(len(a) == 9)
-            number = int(a[-1])
-            assert(a[-3] == 'HTTP/1.1"')
-            assert(a[-4] == '/projects/260')
-            assert(a[-5] == '"GET')
+        else:
+            continue
+        cond1 = (len(a) == 9)
+        number = int(a[-1])
+        cond2 = (a[-3] == 'HTTP/1.1"')
+        cond3 = (a[-4] == '/projects/260')
+        cond4 = (a[-5] == '"GET')
+        if (cond1 and cond2 and cond3 and cond4):
             currentStats[code] = currentStats.get(code, 0) + 1
             fileSize += number
             lineNumber += 1
-        except KeyboardInterrupt:
-            print("File size: {}".format(fileSize))
-            for key in sorted(currentStats):
-                print("{}: {}".format(key, currentStats[key]))
-            break
-        except Exception as e:
-            if e == KeyboardInterrupt:
-                pass
         if lineNumber % 10 == 0:
-            print("File size: {}".format(fileSize))
-            for key in sorted(currentStats):
-                print("{}: {}".format(key, currentStats[key]))
-    except KeyboardInterrupt:
-        print("File size: {}".format(fileSize))
-        for key in sorted(currentStats):
-            print("{}: {}".format(key, currentStats[key]))
+            printstats()
+            continue
+    except Exception as e:
+        continue
---
+++ 0x03-log_parsing/test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Run 0-stats.py with simulated data
+./0-generator.py | ./0-stats.py
