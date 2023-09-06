9bd2eeadd100526b910be1b874166887be9e2d91
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-09-06 05:32:12 +0000

    updated for correctly ordered

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 // get star wars films and check number of films that
-// print characters to screen 
+// edge Antilles, character ID 18  is in and return
+// the numbers to console
 const request = require('request');
 const filmId = process.argv[2];
 request('https://swapi-api.alx-tools.com/api/films/' + filmId, function (error, response, body) {
@@ -10,6 +11,8 @@ request('https://swapi-api.alx-tools.com/api/films/' + filmId, function (error, 
   const res = JSON.parse(response.body);
   const characters = res.characters;
   let charUrl;
+  const names = {};
+  let out = 0;
   for (let j = 0; j < characters.length; j++) {
     charUrl = characters[j];
     request(charUrl, function (error, response, body) {
@@ -17,7 +20,13 @@ request('https://swapi-api.alx-tools.com/api/films/' + filmId, function (error, 
         console.error('error', error);
       }
       const data = JSON.parse(response.body);
-      console.log(data.name);
+      names[j] = data.name;
+      out += 1;
+      if (out === characters.length) {
+        for (let i = 0; i < characters.length; i++) {
+          console.log(names[i]);
+        }
+      }
     });
   }
 });
