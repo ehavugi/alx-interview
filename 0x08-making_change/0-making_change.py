f31436b0870f8f712fbb845fa8ed1810d12af027
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-09-24 08:56:16 +0000

    added an early return for optimal number of coins

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -16,7 +16,6 @@ def makeChange(coins, total):
     dictD = {}
     for coin in coins:
         dictD[coin] = (total % coin)
-
     Set = False
     extra = 0
     for value in dictD.values():
@@ -24,14 +23,21 @@ def makeChange(coins, total):
             Set = True
         else:
             extra += value
+
     if not Set and total % extra != 0:
         return -1
+    DictC2 = {}
+    coins = sorted(coins, reverse=True)
     changeUpTo = [float('inf')] * (total + 1)
     changeUpTo[0] = 0
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i-coin >= 0:
+
+    for coin in coins:
+        for i in range(coin, total+1):
+
+            if i >= 0:
                 changeUpTo[i] = min(changeUpTo[i], changeUpTo[i - coin] + 1)
+            if changeUpTo[total] != float('inf'):
+                return changeUpTo[total]
     if changeUpTo[total] == float('inf'):
         return -1
     return changeUpTo[total]
